c7a80389d34d99b671fcaee9d0adbd69fb85ed53
nikamamrut <nikamamrut.16@gmail.com>
2025-01-28 12:33:09 +0530

    updated app to v2

--- hello-nodejs/app.js
+++ hello-nodejs/app.js
@@ -1,2 +1,9 @@
-console.log("Hello World!\n");
+var response;
 
+app.get('/', function (req, res) {
+    response = 'This is version 2 of the app.' + '\n';
+
+    //send the response to the client
+    res.send(response);
+
+});
