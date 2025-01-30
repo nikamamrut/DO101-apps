e5369ecf1eafd215836084551f65264283227958
nikamamrut <nikamamrut.16@gmail.com>
2025-01-30 12:18:54 +0530

    changed

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,3 +1,6 @@
 <?php
   print "I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
 ?>
+<?php
+  print "I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+?>
